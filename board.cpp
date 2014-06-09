e44b1d5e394aa73ed4db78215c2e14cde2daf0fe
bpsilva <bpsilva@inf.ufrgs.br>
2014-06-09 19:40:10 -0300

    movimentos peoes (falta enpassant)

--- board.cpp
+++ board.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 typedef struct bitboard
 {
-long long my_pawns, my_rooks, my_bishops, his_pawns, his_rooks, his_bishops;
+long long white_pawns, white_rooks, white_bishops, black_pawns, black_rooks, black_bishops, enpassant;
 }BOARD;
 
 typedef struct mov
@@ -20,12 +20,12 @@ mov *prox;
 BOARD translation(char sboard[64], int who_moves, BOARD board)
 {
 
-board.my_pawns = 0;
-board.my_rooks = 0;
-board.my_bishops = 0;
-board.his_pawns = 0;
-board.his_rooks = 0;
-board.his_bishops = 0;
+board.white_pawns = 0;
+board.white_rooks = 0;
+board.white_bishops = 0;
+board.black_pawns = 0;
+board.black_rooks = 0;
+board.black_bishops = 0;
 unsigned long long um = 1;
 
 for(int i = 0; i<64; i++)
@@ -37,34 +37,34 @@ switch(sboard[i])
 
 	case 'p':
 		if(who_moves == 1)
-			board.his_pawns = board.his_pawns | (um<<(63-i));
+			board.black_pawns = board.black_pawns | (um<<(63-i));
 		else
-			board.my_pawns = board.my_pawns | (um<<(63-i));
+			board.white_pawns = board.white_pawns | (um<<(63-i));
 	case 'r':
 		if(who_moves == 1)
-			board.his_rooks = board.his_rooks | (um<<(63-i));
+			board.black_rooks = board.black_rooks | (um<<(63-i));
 		else
-			board.my_rooks = board.my_rooks | (um<<(63-i));
+			board.white_rooks = board.white_rooks | (um<<(63-i));
 	case 'b':
 		if(who_moves == 1)
-			board.his_bishops = board.his_bishops | (um<<(63-i));
+			board.black_bishops = board.black_bishops | (um<<(63-i));
 		else
-			board.my_bishops = board.my_bishops | (um<<(63-i));
+			board.white_bishops = board.white_bishops | (um<<(63-i));
 	case 'P':
 		if(who_moves == -1)
-			board.his_pawns = board.his_pawns | (um<<(63-i));
+			board.black_pawns = board.black_pawns | (um<<(63-i));
 		else
-			board.my_pawns = board.my_pawns | (um<<(63-i));
+			board.white_pawns = board.white_pawns | (um<<(63-i));
 	case 'R':
 		if(who_moves == -1)
-			board.his_rooks = board.his_rooks | (um<<(63-i));
+			board.black_rooks = board.black_rooks | (um<<(63-i));
 		else
-			board.my_rooks = board.my_rooks | (um<<(63-i));
+			board.white_rooks = board.white_rooks | (um<<(63-i));
 	case 'B':
 		if(who_moves == -1)
-			board.his_bishops = board.his_bishops | (um<<(63-i));
+			board.black_bishops = board.black_bishops | (um<<(63-i));
 		else
-			board.my_bishops = board.my_bishops | (um<<(63-i));
+			board.white_bishops = board.white_bishops | (um<<(63-i));
 	
 	}
 }
@@ -92,43 +92,45 @@ void print(unsigned long long board)
 unsigned long long movements()
 {
 	///para teste--------
-	BOARD b = {pow(2,8)-1, 0, 0, (pow(2,8)-1), 0, 0};
-	b.his_pawns<<=8;
+	BOARD b = {pow(2,8)-1, 0, 0, 0, 0, 0, 0};
+	b.white_pawns<<=8;
 	///------------------
 
 
-	BOARD newboard = {b.my_pawns, b.my_rooks, b.my_bishops, b.his_pawns, b.his_rooks, b.his_bishops};	
+	BOARD newboard = {b.white_pawns, b.white_rooks, b.white_bishops, b.black_pawns, b.black_rooks, b.black_bishops, b.enpassant};	
 	BOARD mov[100];
 	int mcounter = 0;
 
-	long long my_pieces = b.my_pawns | b.my_bishops;
-	my_pieces = my_pieces | b.my_rooks;
-	my_pieces = -my_pieces;
-	my_pieces--;
+	long long white_pieces = b.white_pawns | b.white_bishops;
+	white_pieces = white_pieces | b.white_rooks;
+	white_pieces = -white_pieces;
+	white_pieces--;
 
-	long long his_pieces = b.his_pawns | b.his_bishops;
-	his_pieces = his_pieces | b.his_rooks;
-	his_pieces = -his_pieces;
-	his_pieces--;
+	long long black_pieces = b.black_pawns | b.black_bishops;
+	black_pieces = black_pieces | b.black_rooks;
+	black_pieces = -black_pieces;
+	black_pieces--;
 
 	long long aux = 0;
-	long long pawn = 1;
+	long long pawn = 0;
 
-// my pawns' movements
+// white pawns' movements
 	
 	//para frente
-	aux = b.my_pawns;
+	aux = b.white_pawns;
 	aux <<= 8;
-	aux &=my_pieces;
-	aux &=his_pieces;
-	newboard.my_pawns = b.my_pawns;
+	aux &=white_pieces;
+	aux &=black_pieces;
+	newboard.white_pawns = b.white_pawns;
 	//pawn = menos signigicativo de aux
 	pawn = aux&-aux;
+
+		//movimentos para frente
 
 	while(pawn!=0)
 	{
 		//adiciona pawn em newboard.pawns
-		newboard.my_pawns|=pawn;
+		newboard.white_pawns|=pawn;
 		//apaga pawn de aux e elimina peças do oponente
 		pawn = -pawn;
 		pawn--;
@@ -139,89 +141,125 @@ unsigned long long movements()
 		pawn>>=8;
 		pawn = -pawn;
 		pawn--;
-		newboard.my_pawns&=pawn;
+		newboard.white_pawns&=pawn;
 		pawn = -pawn;
 		pawn--;
 		mov[mcounter++] = newboard;
+		
+		if(pawn<=pow(2,48)-1)
+		{
 
-		newboard.my_pawns = b.my_pawns;
+			pawn <<= 16;
+			black_pieces = -black_pieces;
+			black_pieces--;
+			white_pieces = -white_pieces;
+			white_pieces--;
+			
+			if((pawn & black_pieces) == 0 && (pawn & white_pieces) == 0)
+			{
+				newboard.white_pawns|=pawn;
+				pawn >>= 8;
+				pawn = -pawn;
+				pawn--;
+				newboard.white_pawns&=pawn;
+				pawn = -pawn;
+				pawn--;
+
+				if(pow(2,23)>=pawn && pawn >=pow(2,16))
+				{
+
+				pawn <<= 8;
+				
+					newboard.enpassant = pawn;
+				}
+				mov[mcounter++] = newboard;
+				
+			}		
+			black_pieces = -black_pieces;
+			black_pieces--;
+			white_pieces = -white_pieces;
+			white_pieces--;	
+		}
+		newboard.white_pawns = b.white_pawns;
+		newboard.enpassant = b.enpassant;
 		//pawn = menos signigicativo de aux
 		pawn = aux&-aux;
-		
 	}
 	
-	his_pieces = -his_pieces;
-	his_pieces--;
+	black_pieces = -black_pieces;
+	black_pieces--;
 
 	//para diagonal esquerda
-	aux = b.my_pawns;
+	aux = b.white_pawns;
 	aux&=9187201950435737471;
 	aux <<= 9;
-	aux &=my_pieces;
-	aux &=his_pieces;
-	newboard.my_pawns = b.my_pawns;
+	aux &=white_pieces;
+	aux &=black_pieces;
+	newboard.white_pawns = b.white_pawns;
+
 	//pawn = menos signigicativo de aux
 	pawn = aux&-aux;
+
 	while(pawn!=0)
 	{
 		//adiciona pawn em newboard.pawns
-		newboard.my_pawns|=pawn;
+		newboard.white_pawns|=pawn;
 		//apaga pawn de aux e elimina peças do oponente
 		pawn = -pawn;
 		pawn--;
 		aux &=pawn;
-		newboard.his_pawns&=pawn;		
-		newboard.his_bishops&=pawn;
-		newboard.his_rooks&=pawn;
+		newboard.black_pawns&=pawn;		
+		newboard.black_bishops&=pawn;
+		newboard.black_rooks&=pawn;
 		pawn = -pawn;
 		pawn--;
 		//remover pawn da posição original em newboard.pawns
 		pawn>>=9;
 		pawn = -pawn;
 		pawn--;
-		newboard.my_pawns&=pawn;
+		newboard.white_pawns&=pawn;
 		pawn = -pawn;
 		pawn--;
 		mov[mcounter++] = newboard;
 
-		newboard.my_pawns = b.my_pawns;
+		newboard.white_pawns = b.white_pawns;
 		//pawn = menos signigicativo de aux
 		pawn = aux&-aux;
 		
 	}
 
 	//para diagonal direita
-	aux = b.my_pawns;
+	aux = b.white_pawns;
 	aux&=-72340172838076674;
 	aux <<= 7;
-	aux &=my_pieces;
-	aux &=his_pieces;
-	newboard.my_pawns = b.my_pawns;
+	aux &=white_pieces;
+	aux &=black_pieces;
+	newboard.white_pawns = b.white_pawns;
 	//pawn = menos signigicativo de aux
 	pawn = aux&-aux;
 	while(pawn!=0)
 	{
 		//adiciona pawn em newboard.pawns
-		newboard.my_pawns|=pawn;
+		newboard.white_pawns|=pawn;
 		//apaga pawn de aux e elimina peças do oponente
 		pawn = -pawn;
 		pawn--;
 		aux &=pawn;
-		newboard.his_pawns&=pawn;		
-		newboard.his_bishops&=pawn;
-		newboard.his_rooks&=pawn;
+		newboard.black_pawns&=pawn;		
+		newboard.black_bishops&=pawn;
+		newboard.black_rooks&=pawn;
 		pawn = -pawn;
 		pawn--;
 		//remover pawn da posição original em newboard.pawns
 		pawn>>=7;
 		pawn = -pawn;
 		pawn--;
-		newboard.my_pawns&=pawn;
+		newboard.white_pawns&=pawn;
 		pawn = -pawn;
 		pawn--;
 		mov[mcounter++] = newboard;
 
-		newboard.my_pawns = b.my_pawns;
+		newboard.white_pawns = b.white_pawns;
 		//pawn = menos signigicativo de aux
 		pawn = aux&-aux;
 		
@@ -230,7 +268,9 @@ unsigned long long movements()
 
 	for(int i =0;i<mcounter;i++)
 	{
-	print(mov[i].my_pawns);
+	print(mov[i].white_pawns);
+	printf("\n");
+	print(mov[i].enpassant);
 	printf("\n");
 	
 	}
@@ -243,5 +283,6 @@ int main()
 
 {
 movements();
+
 return 0;
 }
